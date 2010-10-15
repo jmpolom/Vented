558f0a20fb1426ed961b206800772e0c153ec24c
Jon Polom <s0nic0nslaught@gmail.com>
2010-10-15 12:09:59 -0400

    Added impedance calculation features

--- test.py
+++ test.py
@@ -4,13 +4,16 @@ import matplotlib.pyplot as plt
 import vented
 
 # User specified design parameters
-Fs = 32.0  # Driver resonance frequency, in Hertz
-Qts = 0.26 # Total driver Q, dimensionless
-Vas = 2.76 # Driver air compliance volume, in cubic feet
-Lv = 4.0   # Enclosure vent length, in inches
-R = 0.75   # Enclosure vent internal radius, in inches
-Vb = 0.75  # Enclosure volume, in cubic feet
-Ql = 7     # Enclosure loss factor
+Fs = 26.2  # Driver resonance frequency, in Hertz
+Qes = 0.39 # Driver electrical Q
+Qms = 9.91 # Driver mechanical Q
+Re = 3.5   # Driver DC resistance
+Vas = 4.38 # Driver air compliance volume, in cubic feet
+Xmax = 9.84# Driver Xmax
+Lv = 12.0  # Enclosure vent length, in inches
+R = 2.00   # Enclosure vent internal radius, in inches
+Vb = 2.89  # Enclosure volume, in cubic feet
+# Use default enclosure loss factor, Ql=7
 
-vented.response_plot(Fs,Qts,Vas,Lv,R,Vb,Ql,plot=plt)
+vented.response_plot(Fs,Qes,Qms,Re,Xmax,Vas,Vb,Lv,R,freq_max=500,plot=plt)
 plt.show()
--- vented.py
+++ vented.py
@@ -16,7 +16,7 @@ __author__ = 'Jonathan Polom <s0nic0nslaught@gmail.com>'
 __date__ = datetime.date(2010, 10, 13)
 __version__ = 0.3
 
-def dependent_params(Fs,Qts,Vas,Vb,Lv,R):
+def params(Fs,Qes,Qms,Vas,Vb,Lv,R):
     """
     Calculates dependent system parameters: enclosure tuning frequency, system tuning ratio, system compliance ratio, and the inverted driver resonance angular frequency
 
@@ -30,6 +30,8 @@ def dependent_params(Fs,Qts,Vas,Vb,Lv,R):
         System tuning ratio
     Qt : float
         Total system Q
+    Tb : float
+        Inverted system tuning angual frequency
     Ts : float
         Inverted driver resonance angular frequency
     """
@@ -37,12 +39,13 @@ def dependent_params(Fs,Qts,Vas,Vb,Lv,R):
     Fb = np.sqrt((1.463e7*R)/(Vb_ci * (Lv + 1.463*R))) # Needs volume in in.^3
     h = Fb/Fs # Tuning ratio
     a = Vas/Vb # Compliance ratio
-    Ts = (2*np.pi*Fs)**-1
+    Tb = 1/(2*np.pi*Fb)
+    Ts = 1/(2*np.pi*Fs)
 
     # Approximate total system Q (Qt) with total driver Q (Qts)
-    Qt = Qts
+    Qt = Qes*Qms/(Qes + Qms)
 
-    return a,Fb,h,Qt,Ts
+    return a,Fb,h,Qt,Tb,Ts
 
 def alignment_spec(a,h,Ql,Qt,Ts):
     """    
@@ -72,11 +75,54 @@ def response(f,T0,a1,a2,a3):
         Relative system response gain, in decibels
     """
     s = complex(0,2*np.pi*f)
+
     num = s**4 * T0**4
     denom = s**4 * T0**4 + a1 * s**3 * T0**3 + a2 * s**2 * T0**2 + a3 * s * T0 + 1
     gain = np.sqrt((num/denom).real**2 + (num/denom).imag**2)
     dB = 10*np.log10(gain)
+
     return dB
+
+def displacement(f,a,Ql,Qt,Tb,Ts,Xmax):
+    """
+    Driver displacement function, equation (14) in 'Vented Box Loudspeaker Systems'
+
+    Returns
+    -------
+    dB : float
+        Normalized displacement value
+    """
+
+    s = complex(0,2*np.pi*f)
+
+    num = s**2*Tb**2 + s*Tb/Ql + 1
+    denom = s**4*Tb**2*Ts**2 + s**3*(Tb**2*Ts/Qt + Tb*Ts**2/Ql) + s**2*((a + 1)*Tb**2 + (Tb*Ts)/(Ql*Qt) + Ts**2) + s*(Tb/Ql + Ts/Qt) + 1
+
+    displacement = Xmax*np.sqrt((num/denom).real**2 + (num/denom).imag**2)
+#    dB = 10*np.log10(displacement)
+
+    return displacement
+
+def impedance(f,a,Ql,Qes,Qms,Re,Tb,Ts):
+    """
+    Voice coil impedance function, equation (16) in 'Vented Box Loudspeaker Systems'
+
+    Returns
+    -------
+    impedance : float
+        Voice coil impedance value, in ohms
+    """
+
+    s = complex(0,2*np.pi*f)
+
+    num = s*(Ts/Qms) + (s**2*Tb**2 + s*Tb/Ql + 1)
+    denom = s**4*Tb**2*Ts**2 + s**3*(Tb**2*Ts/Qms + Tb*Ts**2/Ql) + s**2*((a + 1)*Tb**2 + (Tb*Ts)/(Ql*Qms) + Ts**2) + s*(Tb/Ql + Ts/Qms) + 1
+
+    Res = Re*(Qms/Qes)
+
+    impedance = float(Re) + Res*np.sqrt((num/denom).real**2 + (num/denom).imag**2)
+
+    return impedance
 
 def xtickmarks(xmin,xmax):
     """
@@ -114,7 +160,7 @@ def xtickmarks(xmin,xmax):
     return loc[imin:imax + 1],labels[imin:imax + 1]
 
 
-def response_plot(Fs,Qts,Vas,Lv,R,Vb,Ql,freq_min=10,freq_max=20000,res=1000,plot=None):
+def response_plot(Fs,Qes,Qms,Re,Xmax,Vas,Vb,Lv,R,Ql=7,freq_min=10,freq_max=20000,res=1000,plot=None):
     """
     Calculates and returns the vented loudspeaker enclosure system's response (gain) values over the specified frequency range (default range is 10 Hz to 20 kHz).
 
@@ -126,7 +172,7 @@ def response_plot(Fs,Qts,Vas,Lv,R,Vb,Ql,freq_min=10,freq_max=20000,res=1000,plot
         Relative system response gain values, calculated at frequencies in response_range
     """
     # Generate dependent parameters
-    a,Fb,h,Qt,Ts = dependent_params(Fs,Qts,Vas,Vb,Lv,R)
+    a,Fb,h,Qt,Tb,Ts = params(Fs,Qes,Qms,Vas,Vb,Lv,R)
 
     # Generate alignment specification parameters
     T0,a1,a2,a3 = alignment_spec(a,h,Ql,Qt,Ts)
@@ -137,24 +183,40 @@ def response_plot(Fs,Qts,Vas,Lv,R,Vb,Ql,freq_min=10,freq_max=20000,res=1000,plot
     # Empty array for frequency response data calculated next
     response_values = np.array([])
 
+    # Empty array for displacement data calculated next
+    displacement_values = np.array([])
+
+    # Empty array for impedance data calculated next
+    impedance_values = np.array([])
+
     # Generate frequency response values
     for f in response_range:
         response_values = np.append(response_values,response(f,T0,a1,a2,a3))
+        displacement_values = np.append(displacement_values,displacement(f,a,Ql,Qt,Tb,Ts,Xmax))
+        impedance_values = np.append(impedance_values,impedance(f,a,Ql,Qes,Qms,Re,Tb,Ts))
 
     # Plot the response data if given matplotlib plotting object
     if plot != None:
-        plot.semilogx(response_range,response_values,basex=10)
+        plot.semilogx(response_range,response_values,'b-',basex=10)
+       
         #plot.grid(b=True,which='both') # Doesn't display major and minor gridlines
         plot.grid(b=True,which='minor') # Apparently the only way to get minor and major
         plot.grid(b=True,which='major') # grid lines on the plot...
 
-        # Title
-        plot.title('Vented Enclosure Frequency Response')
-
-        # Y-axis properties
+        # Frequency response Y-axis properties
         plot.ylim(-24,6)
         plot.yticks(range(-24,9,3))
         plot.ylabel('Response Level (dB)')
+
+        # Create second Y-axis for driver displacement
+        plot2x = plot.twinx()
+        plot2x.semilogx(response_range,impedance_values,'k-',linewidth=0.5,basex=10)
+
+        # Driver displacement Y-axis properties
+        plot2x.set_ylabel('Voice Coil Impedance (Ohms)')
+
+        # Title
+        plot.title('Vented Enclosure Frequency Response')
 
         # X-axis properties
         plot.xlim(freq_min,freq_max)
