2e5a77e15440df000733cfd114b67142a93864f4
Jon <s0nic@SiriusA.(none)>
2010-10-19 07:36:57 -0400

    Corrected units mentioned in test.py

--- test.py
+++ test.py
@@ -11,10 +11,10 @@ Fs = 26.2   # Driver resonance frequency, in Hertz
 Qes = 0.39  # Driver electrical Q
 Qms = 9.91  # Driver mechanical Q
 Re = 3.5    # Driver DC resistance
-Vas = 123.9 # Driver air compliance volume, in cubic feet
+Vas = 123.9 # Driver air compliance volume, in liters
 Lv = 10.0   # Enclosure vent length, in inches
 D = 4.0     # Enclosure vent internal radius, in inches
-Vb = 81.84  # Enclosure volume, in cubic feet
+Vb = 81.84  # Enclosure volume, in liters
 # Use default enclosure loss factor, Ql=7
 
 vented.response_plot(Fs,Qes,Qms,Re,Vas,Vb,Lv,D,name=name,freq_max=500,plot=plt)
