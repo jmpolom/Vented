75001230867066b4cc2eae2f5274201e4f54d485
Jon Polom <s0nic0nslaught@gmail.com>
2010-10-21 10:55:46 -0400

    Separated response,impedance and displacement plotting functions

--- vented.py
+++ vented.py
@@ -16,7 +16,7 @@ __author__ = 'Jonathan Polom <s0nic0nslaught@gmail.com>'
 __date__ = datetime.date(2010, 10, 19)
 __version__ = 0.6
 
-def params(Fs,Qes,Qms,Vas,Vb,Lv,D):
+def params_calc(Fs,Qes,Qms,Vas,D,Vb,Lv):
     """
     Calculates dependent system parameters: enclosure tuning frequency, system tuning ratio, system compliance ratio, and the inverted driver resonance angular frequency
 
@@ -39,16 +39,49 @@ def params(Fs,Qes,Qms,Vas,Vb,Lv,D):
     R = D/2 # Convert vent diameter to radius
 
 #    Fb = np.sqrt((1.463e7*R)/(Vb_ci * (Lv + 1.463*R))) # Needs volume in in.^3
-    Fb = np.sqrt((1.464e7*R)/(Vb_ci*Lv)) # Needs volume in in.^3
-    h = Fb/Fs # Tuning ratio
-    a = Vas/Vb # Compliance ratio
-    Tb = 1/(2*np.pi*Fb)
+    Fb = np.sqrt((1.464e7*R)/(Vb_ci*Lv)) # Fb from port. Needs volume in in.^3
+    Tb = 1/(2*np.pi*Fb) # Inverse of angular frequencies
     Ts = 1/(2*np.pi*Fs)
 
-    # Approximate total system Q (Qt) with total driver Q (Qts)
-    Qt = Qes*Qms/(Qes + Qms)
+    h = Fb/Fs # Tuning ratio
+    a = Vas/Vb # Compliance ratio
 
-    return a,Fb,h,Qt,Tb,Ts
+    Qt = Qes*Qms/(Qes + Qms) # Approximate total system Q (Qt) with total driver Q (Qts)
+
+    return Fb,Qt,Tb,Ts,a,h
+
+def params_align(Fs,Qes,Qms,Vas,D,a,h):
+    """
+    Calculates dependent system parameters: enclosure tuning frequency, system tuning ratio, system compliance ratio, and the inverted driver resonance angular frequency
+
+    Returns
+    -------
+    Fb : float
+        Enclosure tuning frequency
+    Lv : float
+        Vent length, in units of vent diameter
+    Qt : float
+        Total system Q
+    Tb : float
+        Inverted system tuning angual frequency
+    Ts : float
+        Inverted driver resonance angular frequency
+    """
+
+    R = D/2 # Convert vent diameter to radius
+
+    Fb = h*Fs # Calculate enclosure tuning frequency from tuning ratio
+    Tb = 1/(2*np.pi*Fb) # Inverse of angular frequencies
+    Ts = 1/(2*np.pi*Fs)
+
+    Vb = Vas/a # Compliance ratio
+
+    Vb_ci = 1000/(2.54**3)*Vb # Convert enclosure volume from liters to in.^3
+    Lv = (1.464e7*R)/(Vb_ci*Fb**2) # Vent length calculation, in units of R
+
+    Qt = Qes*Qms/(Qes + Qms) # Approximate total system Q (Qt) with total driver Q (Qts)
+
+    return Fb,Lv,Qt,Tb,Ts
 
 def alignment_spec(a,h,Ql,Qt,Ts):
     """    
@@ -163,9 +196,9 @@ def xtickmarks(xmin,xmax):
     return loc[imin:imax + 1],labels[imin:imax + 1]
 
 
-def response_plot(Fs,Qes,Qms,Re,Vas,Vb,Lv,D,Ql=7,name='',freq_min=10,freq_max=20000,res=1000,plot=None):
+def response_plot(Fs,Qes,Qms,Re,Vas,D,a=None,h=None,Lv=None,Vb=None,Ql=7,name='',freq_min=10,freq_max=20000,res=1000,plot=None):
     """
-    Calculates and returns the vented loudspeaker enclosure system's response (gain) values over the specified frequency range (default range is 10 Hz to 20 kHz).
+    Calculates and returns a vented loudspeaker enclosure system's response (gain) values over the specified frequency range (default range is 10 Hz to 20 kHz).
 
     Returns
     -------
@@ -182,8 +215,15 @@ def response_plot(Fs,Qes,Qms,Re,Vas,Vb,Lv,D,Ql=7,name='',freq_min=10,freq_max=20
         3) Fs, Qes, Qms, Vas, and D are user-specified, along with a and h -> create alignment using a and h values specified
     """
 
-    # Generate dependent parameters
-    a,Fb,h,Qt,Tb,Ts = params(Fs,Qes,Qms,Vas,Vb,Lv,D)
+    if a is None and h is None and Lv is None and Vb is None:
+        Fb,Lv,Qt,Tb,Ts,a,h = params_lookup(Fs,Qes,Qms,Re,Vas,D)
+    elif Lv is not None and Vb is not None:
+        if a is None and h is None:
+            # Generate dependent params via vent tuning equation
+            Fb,Qt,Tb,Ts,a,h = params_calc(Fs,Qes,Qms,Vas,D,Vb,Lv)
+        elif a is not None and h is not None:
+            # Generate dependent params via specified a,h
+            Fb,Lv,Qt,Tb,Ts = params_align(Fs,Qes,Qms,Vas,D,a,h)
 
     # Generate alignment specification parameters
     T0,a1,a2,a3 = alignment_spec(a,h,Ql,Qt,Ts)
@@ -193,18 +233,18 @@ def response_plot(Fs,Qes,Qms,Re,Vas,Vb,Lv,D,Ql=7,name='',freq_min=10,freq_max=20
 
     # Empty array for frequency response data calculated next
     response_values = np.array([])
-
+    """
     # Empty array for displacement data calculated next
     displacement_values = np.array([])
 
     # Empty array for impedance data calculated next
     impedance_values = np.array([])
-
+    """
     # Generate frequency response values
     for f in response_range:
         response_values = np.append(response_values,response(f,T0,a1,a2,a3))
-        displacement_values = np.append(displacement_values,displacement(f,a,Ql,Qt,Tb,Ts))
-        impedance_values = np.append(impedance_values,impedance(f,a,Ql,Qes,Qms,Re,Tb,Ts))
+#        displacement_values = np.append(displacement_values,displacement(f,a,Ql,Qt,Tb,Ts))
+#        impedance_values = np.append(impedance_values,impedance(f,a,Ql,Qes,Qms,Re,Tb,Ts))
 
     # Plot the response data if given matplotlib plotting object
     if plot != None:
@@ -229,7 +269,7 @@ def response_plot(Fs,Qes,Qms,Re,Vas,Vb,Lv,D,Ql=7,name='',freq_min=10,freq_max=20
         loc,labels = xtickmarks(freq_min,freq_max) # Get tick locations and labels
         plot.xticks(loc,labels)
         plot.xlabel('Frequency (Hz.)')
-
+    """
         # Create second Y-axis for driver displacement
 #        plot2x = plot.twinx()
 #        plot2x.semilogx(response_range,impedance_values,'k-',linewidth=0.5,basex=10)
@@ -264,7 +304,7 @@ def response_plot(Fs,Qes,Qms,Re,Vas,Vb,Lv,D,Ql=7,name='',freq_min=10,freq_max=20
         plot.figure(3)
         plot.semilogx(response_range,displacement_values,basex=10)
 
-        # Figure 2 title
+        # Figure 3 title
         plot.suptitle(name)
         plot.title('Diaphragm Displacement')
         
@@ -272,7 +312,140 @@ def response_plot(Fs,Qes,Qms,Re,Vas,Vb,Lv,D,Ql=7,name='',freq_min=10,freq_max=20
         plot.grid(b=True,which='major')
         
         # Y-axis properties
-        plot.ylabel('Displacement Fraction')
+        plot.ylabel('Displacement Function Magnitude')
+        
+        # X-axis properties
+        plot.xlim(freq_min,freq_max)
+        loc,labels = xtickmarks(freq_min,freq_max) # Get tick locations and labels
+        plot.xticks(loc,labels)
+        plot.xlabel('Frequency (Hz.)')
+    """
+    # Return system response data
+    return response_range,response_values
+
+def impedance_plot(Fs,Qes,Qms,Re,Vas,D,a=None,h=None,Lv=None,Vb=None,Ql=7,name='',freq_min=10,freq_max=20000,res=1000,plot=None):
+    """
+    Calculates and returns a vented loudspeaker enclosure driver's impedance values over the specified frequency range (default range is 10 Hz to 20 kHz).
+
+    Returns
+    -------
+    impedance_range : array-like
+        Frequencies system response was calculated at
+    impedance_values : array-like
+        Relative system response gain values, calculated at frequencies in response_range
+    """
+
+    """
+    Three cases:
+        1) Fs, Qes, Qms, Vas, Vb, Lv, and D are user-specified -> create alignment accordingly starting with calculation of Fb from Lv and D
+        2) Fs, Qes, Qms, Vas, and D are user-specified -> lookup relevant values of a and h according to specified alignment type (EX: SC4 and SBB4)
+        3) Fs, Qes, Qms, Vas, and D are user-specified, along with a and h -> create alignment using a and h values specified
+    """
+
+    if a is None and h is None and Lv is None and Vb is None:
+        Fb,Lv,Qt,Tb,Ts,a,h = params_lookup(Fs,Qes,Qms,Re,Vas,D)
+    elif Lv is not None and Vb is not None:
+        if a is None and h is None:
+            # Generate dependent params via vent tuning equation
+            Fb,Qt,Tb,Ts,a,h = params_calc(Fs,Qes,Qms,Vas,D,Vb,Lv)
+        elif a is not None and h is not None:
+            # Generate dependent params via specified a,h
+            Fb,Lv,Qt,Tb,Ts = params_align(Fs,Qes,Qms,Vas,D,a,h)
+    
+    # Generate alignment specification parameters
+#    T0,a1,a2,a3 = alignment_spec(a,h,Ql,Qt,Ts)
+
+    # Frequency range
+    impedance_range = np.logspace(np.log10(freq_min),np.log10(freq_max),num=res,base=10)
+
+    # Empty array for impedance data calculated next
+    impedance_values = np.array([])
+
+    # Generate frequency response values
+    for f in impedance_range:
+        impedance_values = np.append(impedance_values,impedance(f,a,Ql,Qes,Qms,Re,Tb,Ts))
+
+    # Plot the impedance data if given matplotlib plotting object
+    if plot != None:
+        # Figure 2 for impedance plot
+        plot.figure(2)
+        plot.semilogx(impedance_range,impedance_values,basex=10)
+
+        # Figure 2 title
+        plot.suptitle(name)
+        plot.title('Voice Coil Impedance')
+
+        plot.grid(b=True,which='minor')
+        plot.grid(b=True,which='major')
+
+        # Frequency response Y-axis properties
+        plot.ylabel('Impedance (Ohms)')
+
+        # X-axis properties
+        plot.xlim(freq_min,freq_max)
+        loc,labels = xtickmarks(freq_min,freq_max) # Get tick locations and labels
+        plot.xticks(loc,labels)
+        plot.xlabel('Frequency (Hz.)')
+
+    return impedance_range,impedance_values
+
+def displacement_plot(Fs,Qes,Qms,Re,Vas,D,a=None,h=None,Lv=None,Vb=None,Ql=7,name='',freq_min=10,freq_max=20000,res=1000,plot=None):
+    """
+    Calculates and returns a vented loudspeaker enclosure driver's impedance values over the specified frequency range (default range is 10 Hz to 20 kHz).
+
+    Returns
+    -------
+    displacement_range : array-like
+        Frequencies system response was calculated at
+    displacement_values : array-like
+        Relative system response gain values, calculated at frequencies in response_range
+    """
+
+    """
+    Three cases:
+        1) Fs, Qes, Qms, Vas, Vb, Lv, and D are user-specified -> create alignment accordingly starting with calculation of Fb from Lv and D
+        2) Fs, Qes, Qms, Vas, and D are user-specified -> lookup relevant values of a and h according to specified alignment type (EX: SC4 and SBB4)
+        3) Fs, Qes, Qms, Vas, and D are user-specified, along with a and h -> create alignment using a and h values specified
+    """
+
+    if a is None and h is None and Lv is None and Vb is None:
+        Fb,Lv,Qt,Tb,Ts,a,h = params_lookup(Fs,Qes,Qms,Re,Vas,D)
+    elif Lv is not None and Vb is not None:
+        if a is None and h is None:
+            # Generate dependent params via vent tuning equation
+            Fb,Qt,Tb,Ts,a,h = params_calc(Fs,Qes,Qms,Vas,D,Vb,Lv)
+        elif a is not None and h is not None:
+            # Generate dependent params via specified a,h
+            Fb,Lv,Qt,Tb,Ts = params_align(Fs,Qes,Qms,Vas,D,a,h)
+    
+    # Generate alignment specification parameters
+#    T0,a1,a2,a3 = alignment_spec(a,h,Ql,Qt,Ts)
+
+    # Frequency range
+    displacement_range = np.logspace(np.log10(freq_min),np.log10(freq_max),num=res,base=10)
+
+    # Empty array for impedance data calculated next
+    displacement_values = np.array([])
+
+    # Generate frequency response values
+    for f in displacement_range:
+        displacement_values = np.append(displacement_values,displacement(f,a,Ql,Qt,Tb,Ts))
+
+    # Plot the impedance data if given matplotlib plotting object
+    if plot != None:
+        # Figure 3 for diaphragm displacement plot
+        plot.figure(3)
+        plot.semilogx(displacement_range,displacement_values,basex=10)
+
+        # Figure 3 title
+        plot.suptitle(name)
+        plot.title('Diaphragm Displacement')
+        
+        plot.grid(b=True,which='minor')
+        plot.grid(b=True,which='major')
+        
+        # Y-axis properties
+        plot.ylabel('Displacement Function Magnitude')
         
         # X-axis properties
         plot.xlim(freq_min,freq_max)
@@ -280,5 +453,4 @@ def response_plot(Fs,Qes,Qms,Re,Vas,Vb,Lv,D,Ql=7,name='',freq_min=10,freq_max=20
         plot.xticks(loc,labels)
         plot.xlabel('Frequency (Hz.)')
 
-    # Return system response data
-    return response_range,response_values,impedance_values,displacement_values
+    return displacement_range,displacement_values
